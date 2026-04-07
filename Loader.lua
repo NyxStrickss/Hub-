b2fff08dd7218b8a9a905490b0c3bbc94a4eb230
NyxStrickss <ezequielbalbuena66567@gmail.com>
2026-04-07 00:44:32 -0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,17 +1,23 @@
---// LOADER
+--// LOADER FINAL PRO (FIXED LINKS)
 
 local Modules = {}
 
-Modules.Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/refs/heads/main/Core/Config.lua"))()
-Modules.UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/refs/heads/main/Core/UI.lua"))()
-Modules.Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/refs/heads/main/Core/Notify.lua"))()
+Modules.Login = loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/main/Core/Login.lua"))()
+Modules.Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/main/Core/Config.lua"))()
+Modules.UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/main/Core/UI.lua"))()
+Modules.Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/main/Core/Notify.lua"))()
 
--- Init UI
-local UI = Modules.UI:Init(Modules.Config)
+-- 🔐 LOGIN
+Modules.Login:Start(function()
 
--- Load Modules
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/refs/heads/main/Modules/Visual.lua"))()(UI, Modules)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/refs/heads/main/Modules/Radar.lua"))()(UI, Modules)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/refs/heads/main/Modules/Player.lua"))()(UI, Modules)
+	-- 🚀 INIT UI
+	local UI = Modules.UI:Init(Modules.Config)
 
-Modules.Notify("Hub cargado correctamente 🧠")
+	-- 📦 LOAD MODULES
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/main/Modules/Visual.lua"))()(UI, Modules)
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/main/Modules/Radar.lua"))()(UI, Modules)
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NyxStrickss/Hub-/main/Modules/Player.lua"))()(UI, Modules)
+
+	Modules.Notify("Hub cargado correctamente 🧠")
+
+end)
